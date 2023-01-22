bc1d5833000fc732fdd284f38911f7b7e1115463
zaouich <121829336+zaouich@users.noreply.github.com>
2023-01-22 18:28:35 +0100

    security
    rate limiting +http headers + data sanitization , parameters' pollution

--- app.js
+++ app.js
@@ -6,15 +6,36 @@ const productsRouter = require("./routers/productsRouter");
 const reviewsRouter = require("./routers/reveiwsRouter");
 const CartProductsRouter = require("./routers/cartProductsRouter");
 const errController = require("./controllers/errController");
-const bodyParser = require("body-parser");
+// security
+const helmet = require("helmet");
+const rateLmit = require("express-rate-limit");
+const sanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const AppError = require("./utils/AppError");
+const catchAsync = require("./utils/catchAsync");
 const app = express();
-app.use(express.json());
 
+app.use(helmet()); // http
+const limiter = rateLmit({
+	max: 100,
+	windowMs: 60 * 60 * 1000,
+	message: "too many reqs on one hour ",
+}); // rating limit
+app.use(express.json());
+app.use(xss()); // html
+app.use(sanitize()); // no sql
+app.use(
+	hpp({
+		whitelist: ["price", "categorie", "image", "description", "name"],
+	})
+); // api featurs
+app.use(limiter);
 app.use("/api/v1/users", usersRouter);
 app.use("/api/v1/products", productsRouter);
 app.use("/api/v1/reviews", reviewsRouter);
 app.use("/api/v1/cartProducts", CartProductsRouter);
-app.all("*", () => console.log("no page found"));
+app.all("*", (req, res) => res.status(204).send("ops..204"));
 app.use(errController);
 module.exports = app;
 //
